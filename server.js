01bfca9bb12f1f91e4f028bf19cd039aac6d84f0
Aman Yadav <amanyadav04st@gmail.com>
2023-01-15 13:57:39 +0530

    serverchange

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 const db = require("./config/db");
 app.use(express.json());
-// var axios = require("axios").default;
-const axios = require("axios");
+const userRoute = require('./routes/userRoutes');
+const companyRoute = require("./routes/companyRoute");
+const uploadCSVRoute = require('./routes/uploadCSVRoute')
+const cors = require("cors");
 
-// const axios = require("axios");
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(cors());
 
-const options = {
-  method: 'GET',
-  url: 'https://latest-stock-price.p.rapidapi.com/price',
-  params: {Indices: 'NIFTY 50'},
-  headers: {
-    'X-RapidAPI-Key': 'e247af8dc7msh89f63f09cebed06p19dbbdjsn7ac29b02e373',
-    'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
-  }
-};
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+app.use("/api/users", userRoute);
+app.use("/api/companyData", companyRoute);
+// app.use("/api/csv", uploadCSVRoute);
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`backend is running fine on port ${port}`);
 });
-
